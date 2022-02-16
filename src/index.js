439a56093ebb43bb7d753c7b35f08d3ff32b27a2
pavi-ravi26 <pavithraieni.20200530@iit.ac.lk>
2022-02-16 12:16:28 +0530

    landing page created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import Start from './pages/Start';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Start />
   </React.StrictMode>,
   document.getElementById('root')
 );
