47165fe52926a146615047aaf08768f85090c737
kaveendra wijenayake <65034521+kaveendra99@users.noreply.github.com>
2022-02-16 22:50:47 +0530

    Navigation Bar has created

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Statistics',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Matches',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
