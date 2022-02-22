94ba714f9009d3fc4dbf9ac31fe3077effd1eb26
pavi-ravi26 <pavithraieni.20200530@iit.ac.lk>
2022-02-22 07:31:09 +0530

    Landing Page updated

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,18 +1,30 @@
-// import React from 'react';
-//import cricket from './Images/LP img1.jpg';
+import React from 'react';
+//import cricket from '../assets/LP img1.jpg';
+//import {link} from 'react-router-dom';
+import { styled } from '@mui/material/styles'
 import '../styles/Start.css';
-//import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+
 
 function Start() {
   return (
-    <div className="App">
-        <div className="text">
-          <p id="txt1">
-             BATSMEN SCORE PREDICTOR
-          </p>
-          <button className="but1" >Get Started</button>  
-        </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+           <img alt="complex" src="../assets/LPimg1.jpg" />
+        </Grid>
+        <Grid item xs={9} container direction="column" justifyContent="center" alignItems="center">
+          <div className="text">
+             <p id= "txt1">BATSMEN SCORE PREDICTOR</p>
+             <Button className="but1" variant="outlined" color="primary">Get Started</Button>
+          </div>
+          </Grid>
+      </Grid>
+    </Box>
+    
   );
 }
 
