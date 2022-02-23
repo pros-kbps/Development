f0930ee481b2291d9f74fe845009eed1a7772536
Buhushika Kumarage <buhushika.2019731@iit.ac.lk>
2022-02-23 19:11:21 +0530

    added image and did some styling changes to form

--- src/components/StyledPaper.js
+++ src/components/StyledPaper.js
@@ -4,9 +4,11 @@ import { withStyles } from "@material-ui/core/styles";
 const StyledPaper = withStyles((theme) => ({
     root: {
         padding: 10,
-        margin: theme.spacing(35),
+        margin: theme.spacing(10),
         padding: theme.spacing(10),
         paddingTop: 50,
+        backgroundColor:"#ADD8E6",
+        width:'25%',
 
     }
 }))(Paper);
