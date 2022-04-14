f3841d71809e2af489a41fd9230fc93eb01ee310
prayon1953 <94886527+prayon1953@users.noreply.github.com>
2022-04-14 15:56:05 +0530

    About us page
    updated the about.js, about.css and app.js and added other relevant images and files. completed the about us page.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import {Route} from 'react-router-dom';
 import Start from './pages/Start';
 import Home from './pages/HomePage/Home';
+import About from './pages/About';
 import Prediction from './pages/PredictionPage/Prediction';
 import './App.css';
 
@@ -19,6 +20,9 @@ function App() {
           <Route path="/prediction">
               <Prediction/> 
           </Route> 
+          <Route path="/About">
+              <About/>
+          </Route>
 
       </div>
 
